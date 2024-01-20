707b83df4f4578db7e6663c881d66eaa1ad7d9ca
Vincent Ockers <vincentbockers@gmail.com>
2024-01-20 12:27:41 +0100

    added run_exit to builtins.h

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -4,8 +4,9 @@
 # include "env.h"
 
 int	run_echo(char **args);
-int	run_pwd(void);
-int	run_export(t_env **env, char ***envp, char **args);
 int	run_env(char **envp);
+int	run_exit(char **args);
+int	run_export(t_env **env, char ***envp, char **args);
+int	run_pwd(void);
 
 #endif
