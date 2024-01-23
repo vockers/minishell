0efbb1653b9f4ac1780dac327e7b1d59527ac5c1
Vincent Ockers <vockers@f0r1s11.codam.nl>
2024-01-23 16:29:18 +0100

    main function

---
+++ main/main.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+
+int	gl_sig = -1;
+
+int	main(int ac, char *argv[], char **envp)
+{
+	t_mini		ms;
+	t_parser	*parser;
+	char		*line;
+
+	if (!mini_init(&ms, envp))
+		return (1);
+	suppress_output();
+	while (1)
+	{
+		signal_handler();
+		rl_catch_signals = 0;
+		line = readline("msh> ");
+		if (!line)
+			return (0);
+		if (ft_strcmp(line, "") == 0)
+		{
+			free(line);
+			continue;
+		}
+		parser = parser_init(line);	
+		parser_parse(parser);
+		exe_line(parser->ast);
+		ast_destroy(parser->ast);
+		free(parser);
+		free(line);
+	}
+	mini_cleanup(&ms);
+	return (0);
+}
---
+++ main/minishell.h
@@ -0,0 +1,21 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "builtins.h"
+# include "env.h"
+# include "execute.h"
+# include "parser.h"
+# include "signals.h"
+
+typedef struct s_mini
+{
+	t_env		*env;
+	char		**envp;
+	bool		exit;
+	int			status;
+}	t_mini;
+
+int		mini_init(t_mini *ms, char **envp);
+void	mini_cleanup(t_mini *ms);
+
+#endif
