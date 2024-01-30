f848adbe9253c6b2a1e91843f21f7cf16712ea93
Julia Caro <jcaro@f0r4s20.codam.nl>
2024-01-30 12:42:07 +0100

    fix new line

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: jcaro <jcaro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 16:42:12 by vockers           #+#    #+#             */
-/*   Updated: 2024/01/30 12:36:06 by jcaro            ###   ########.fr       */
+/*   Updated: 2024/01/30 12:41:00 by jcaro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,18 +23,18 @@ int	run_exit(char **args, t_mini *ms)
 	{
 		if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
 			ft_dprintf(STDERR_FILENO, "minishell: exit: "
-				"%s: numeric argument required", args[1]);
+				"%s: numeric argument required\n", args[1]);
 		else
 			ft_dprintf(STDERR_FILENO, "minishell: exit: "
-				"%s: numeric argument required\n", args[1]);
+				"%s: numeric argument required", args[1]);
 		return (2);
 	}
 	else if (args[1] && args[2])
 	{
 		if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
-			ft_dprintf(STDERR_FILENO, "minishell: exit: too many arguments");
-		else
 			ft_dprintf(STDERR_FILENO, "minishell: exit: too many arguments\n");
+		else
+			ft_dprintf(STDERR_FILENO, "minishell: exit: too many arguments");
 		return (1);
 	}
 	else if (args[1])
